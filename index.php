60bf2c9da6891e2a900effd3ad212bd7a4652b05
Markes <markovandrew@yandex.ru>
2017-04-26 21:07:09 +0700

    The basics syntax of PHP

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-
-phpinfo();
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+            <title>
+                Урок 2
+            </title>
+        </meta>
+    </head>
+    <body>
+        <?php
+// однострочный комментарий
+# также однострочный комментарий
+/*
+это многострочный комментарий
+ */
+echo '<p>Hello World!</p>'; ?>
+    </body>
+</html>
